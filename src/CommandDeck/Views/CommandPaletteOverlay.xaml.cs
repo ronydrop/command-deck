3a10150fa050f875413d3d998ddbac789c98bd31
Rony Oliveira <rony@aprovei.ai>
2026-04-01 18:24:22 -0300

    refactor(D3): extract SearchOverlayControlBase to eliminate code-behind duplication
    Criado Controls/SearchOverlayControlBase.cs como classe base abstrata para
    CommandPaletteOverlay e BranchSelectorOverlay. Encapsula IsVisibleChanged (foco
    no SearchBox), OnKeyDown (Esc/Enter/Up/Down) e OnDimLayerClick. Commands são
    resolvidos por reflexão para evitar acoplamento entre Controls e ViewModels,
    compatível com IRelayCommand e IAsyncRelayCommand gerados pelo source generator.

--- src/CommandDeck/Views/CommandPaletteOverlay.xaml.cs
+++ src/CommandDeck/Views/CommandPaletteOverlay.xaml.cs
@@ -1,12 +1,12 @@
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using CommandDeck.Controls;
 using CommandDeck.Models;
 using CommandDeck.ViewModels;
 
 namespace CommandDeck.Views;
 
-public partial class CommandPaletteOverlay : UserControl
+public partial class CommandPaletteOverlay : SearchOverlayControlBase
 {
     private CommandPaletteViewModel? _vm;
 
@@ -14,48 +14,11 @@ public partial class CommandPaletteOverlay : UserControl
     {
         InitializeComponent();
         DataContextChanged += (_, _) => _vm = DataContext as CommandPaletteViewModel;
-        IsVisibleChanged += (_, e) =>
-        {
-            if ((bool)e.NewValue)
-            {
-                SearchBox.Clear();
-                SearchBox.Focus();
-                Keyboard.Focus(SearchBox);
-            }
-        };
     }
 
-    private void OnKeyDown(object sender, KeyEventArgs e)
+    private void OnResultClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
-        switch (e.Key)
-        {
-            case Key.Escape:
-                _vm?.CloseCommand.Execute(null);
-                e.Handled = true;
-                break;
-            case Key.Enter:
-                _vm?.ConfirmCommand.Execute(null);
-                e.Handled = true;
-                break;
-            case Key.Down:
-                _vm?.MoveDownCommand.Execute(null);
-                e.Handled = true;
-                break;
-            case Key.Up:
-                _vm?.MoveUpCommand.Execute(null);
-                e.Handled = true;
-                break;
-        }
-    }
-
-    private void OnDimLayerClick(object sender, MouseButtonEventArgs e)
-    {
-        _vm?.CloseCommand.Execute(null);
-    }
-
-    private void OnResultClick(object sender, MouseButtonEventArgs e)
-    {
-        if ((sender as FrameworkElement)?.DataContext is CommandDefinitionModel cmd)
+        if ((sender as System.Windows.FrameworkElement)?.DataContext is CommandDefinitionModel cmd)
         {
             if (_vm is not null) _vm.SelectedResult = cmd;
             _vm?.ConfirmCommand.Execute(null);
